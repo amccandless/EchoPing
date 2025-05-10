34eff45fee622169714e829177e6b69f0f6330c5
Tursko <taylor@stykocompany.com>
2025-05-10 14:56:35 -0500

    Testing commit

--- app/src/main/java/com/booknerd/EchoPin/MainActivity.kt
+++ app/src/main/java/com/booknerd/EchoPin/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello person-name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Android 15")
     }
 }
