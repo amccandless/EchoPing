946697f2f08c3800064cc9c50eeeb15c1ecc7c0d
amccandless <amccandless21@student.methodist.edu>
2025-05-10 16:50:31 -0500

    added fields

--- app/src/main/java/com/booknerd/EchoPin/model/Reminder.kt
+++ app/src/main/java/com/booknerd/EchoPin/model/Reminder.kt
@@ -1,6 +1,11 @@
 package com.booknerd.EchoPin.model
 
-class Reminder {
-    val reminderId: Int = 0 //Autogenerate primary key
-    val name = String
-}
+class Reminder(
+    val reminderId: Int,               //Id for reminder
+    val reminderName: String,          //Name of the reminder
+    val latitude: Double,              //Latitude for location
+    val longtitude: Double,            //Longitude for location
+    val radius: Float,                 //Trigger radius in meters
+    val isActive: Boolean = true,      //Whether the reminder is active or not
+
+)
