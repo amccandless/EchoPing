d35eb0df3c442ffb5da9ec7c07dd9271eaa5b399
Taylor <taylor@stykocompany.com>
2025-05-10 17:10:06 -0500

    Create reminder entity, DAO, and app database. (#5)
    * Create reminder entity, DAO, and app database.
    
    * Move ROOM related objects to a new package
    
    * Fix gradle issue for now

---
+++ app/src/main/java/com/booknerd/EchoPin/data/entities/Reminder.kt
@@ -0,0 +1,26 @@
+package com.booknerd.EchoPin.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "reminders")
+data class Reminder(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0,
+
+    @ColumnInfo(name = "title")
+    val title: String?,
+
+    @ColumnInfo(name = "notification_text")
+    val notificationText: String?,
+
+    @ColumnInfo(name = "location_latitude")
+    val locationLatitude: Double,
+
+    @ColumnInfo(name = "location_longitude")
+    val locationLongitude: Double,
+
+    @ColumnInfo(name = "location_radius")
+    val locationRadius: Double,
+)
